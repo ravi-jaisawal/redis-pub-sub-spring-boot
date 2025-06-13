b8a59e081183b4ed477bb73898144e638b998d9c
google <j.ravi11555@gmail.com>
2025-06-13 13:44:47 +0530

    added trade listener

--- src/main/java/org/example/config/RedisConfig.java
+++ src/main/java/org/example/config/RedisConfig.java
@@ -3,6 +3,7 @@ package org.example.config;
 import org.example.constants.RedisQueueNames;
 import org.example.subscriber.RedisSubscriber1;
 import org.example.subscriber.RedisSubscriber2;
+import org.example.subscriber.TradeMessageSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -33,16 +34,20 @@ public class RedisConfig {
     public RedisMessageListenerContainer container(RedisConnectionFactory factory,
                                                    MessageListenerAdapter listener1,
                                                    MessageListenerAdapter listener2,
+                                                   MessageListenerAdapter tradeListener,
                                                    ChannelTopic topicMain,
                                                    ChannelTopic topicRetry) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(factory);
-
+        // listener1
         container.addMessageListener(listener1, topicMain);
         container.addMessageListener(listener1, topicRetry);
-
+        // listener2
         container.addMessageListener(listener2, topicMain);
         container.addMessageListener(listener2, topicRetry);
+        // trade listener
+        container.addMessageListener(tradeListener, topicMain);
+        container.addMessageListener(tradeListener, topicRetry);
 
         return container;
     }
@@ -55,6 +60,11 @@ public class RedisConfig {
     @Bean
     public MessageListenerAdapter listener2(RedisSubscriber2 subscriber2) {
         return new MessageListenerAdapter(subscriber2, "onMessage");
+    }
+
+    @Bean
+    public MessageListenerAdapter tradeAdapter(TradeMessageSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "onMessage");
     }
 
     @Bean
